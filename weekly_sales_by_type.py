e950dd6ae06dae17874dcd2a5a025a5be89eeb29
folukeakpobasa <folukeakpobasa@decagon.dev>
2025-04-05 03:16:44 +0100

    Feat: weekly-sales-by-type

---
+++ weekly_sales_by_type.py
@@ -0,0 +1,5 @@
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(["type", "is_holiday"])["weekly_sales"].sum()
+print(sales_by_type_is_holiday)
